7a12d6c200a1fe965e073ec0a657784398657448
Pawel Kazimierowicz <pawel@kazimierowicz.eu>
2016-11-23 10:41:51 +0100

    Removed noble connect file

--- noble/node-red-contrib-noble.js
+++ noble/node-red-contrib-noble.js
@@ -190,6 +190,6 @@ module.exports = function(RED) {
 
     // Register the node by name. This must be called before overriding any of the
     // Node functions.
-    RED.nodes.registerType("scan ble",NobleScan);
+    RED.nodes.registerType("BLE Scan",NobleScan);
 
 }
